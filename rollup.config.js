cf198b7fcec494db5c493302aa709d7f75669bae
blfunex <blfunex@gmail.com>
2020-08-24 02:05:59 +0100

    Idk what is happening

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,14 @@ import { join } from "path";
 
 const external = Object.keys(Package.dependencies || {});
 
+console.log(process.env);
+
+const DEBUG =
+  process.env.NODE_ENV !== "production" &&
+  (process.env.DEBUG === undefined ||
+    process.env.DEBUG === "true" ||
+    process.env.DEBUG === "");
+
 export default [
   {
     input: join(__dirname, "source/client/main.ts"),
@@ -20,7 +28,7 @@ export default [
     output: {
       file: join(__dirname, "public/bundle.js"),
       format: "esm",
-      sourcemap: process.env.NODE_ENV != "production"
+      sourcemap: DEBUG
     },
 
     plugins: [
@@ -34,7 +42,7 @@ export default [
           }
         },
         ecma: 5,
-        keep_classnames: process.env.NODE_ENV != "production",
+        keep_classnames: DEBUG,
         keep_fnames: false,
         safari10: false,
         ie8: false
